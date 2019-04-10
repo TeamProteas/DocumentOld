6dfb0021b18c164f64f942c72cdc322c4c1bb541
tanejaanmol <anmol.taneja@ninelaps.com>
2019-04-10 11:42:10 +0530

    changes april 10

--- DocumentManagementSystem/src/main/java/com/nineleaps/DocumentManagementSystem/dao/DocumentType.java
+++ DocumentManagementSystem/src/main/java/com/nineleaps/DocumentManagementSystem/dao/DocumentType.java
@@ -8,7 +8,8 @@ import java.util.UUID;
 
 
 @Table("DocumentType")
-public class DocumentType {
+public class
+DocumentType {
 
     @Id
     @Column(value = "uid")
--- DocumentManagementSystem/src/main/java/com/nineleaps/DocumentManagementSystem/dao/EmployeeAccounts.java
+++ DocumentManagementSystem/src/main/java/com/nineleaps/DocumentManagementSystem/dao/EmployeeAccounts.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 
 
 @Table(value="EmployeeAccounts")
-public class EmployeeAccounts {
+public class    EmployeeAccounts {
 
 
 
